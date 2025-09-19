e1422a90571f9d8843557248fa297b91f3a581ae
Harpreet Singh Saluja <hssaluja2508@gmail.com>
2025-09-19 17:56:38 +0530

    added redux

---
+++ src/store/appSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  theme: "light",
+};
+
+const appSlice = createSlice({
+  name: "app",
+  initialState,
+  reducers: {
+    toggleTheme(state) {
+      state.theme = state.theme === "light" ? "dark" : "light";
+    },
+  },
+});
+
+export const { toggleTheme } = appSlice.actions;
+export default appSlice.reducer;
